6864a245348bf1ddced44d0b4547ffe5cf081a73
Arsenii Utlenko <uaganja@gmail.com>
2022-04-07 15:02:53 +0300

    0.0

---
+++ JS/script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+let numberOfFilms;  
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){ 
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    }
+}
+
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+function rememberMyFilms(){
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+rememberMyFilms(); 
+
+function writeYourGenres(){
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres.push(prompt(`Ваш любимый жанр под номером ${i}`, ''));
+    } 
+}    
+
+writeYourGenres();
+
+function detectPersonalLevel(){
+    if (personalMovieDB.count <= 10) {
+        alert('Просмотренно довольно мало фильмов!');
+    } else if (personalMovieDB.count >= 10 || personalMovieDB.count <= 30 ) {
+        alert('Вы классический зритель!');
+    } else if (personalMovieDB.count >= 30) {
+        alert('Вы киноман!');
+    } else {
+        alert('Произошла ошибка');
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden){
+    if (!hidden){
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
